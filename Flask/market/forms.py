c8a5a057f60466b892576eb14f08bf1011604e7d
byaskew3 <byaskew3@gmail.com>
2021-09-30 20:57:03 -0600

    Created Flashes and Validations

--- Flask/market/forms.py
+++ Flask/market/forms.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name: ')
-    email_address = StringField(label='Email Address: ')
-    password1 = PasswordField(label='Password: ')
-    password2 = PasswordField(label='Confirm Password: ')
-    submit = SubmitField(label='submit')
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('This username already exists!')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('This email address already exists!')
+
+    username = StringField(label='User Name:', validators=[Length(min=2, max=15), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField(label='Create Account')
--- Flask/market/routes.py
+++ Flask/market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Champion
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
+from market.models import Champion, User
 from market.forms import RegisterForm
+from market import db
 
 @app.route("/")
 @app.route('/home')
@@ -14,7 +15,17 @@ def market_page():
     champions = Champion.query.all()
     return render_template('market.html', items=champions)
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                                email_address=form.email_address.data,
+                                password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}: # no errors from validations
+        for err_msg in form.errors.values():
+            flash(f'Uh, oh! There was an error creating your account: {err_msg}', category='danger')        
     return render_template('register.html', form=form)
