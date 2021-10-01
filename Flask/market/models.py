1cca931f94c280f89dfe90e193674949cfdee8d0
byaskew3 <byaskew3@gmail.com>
2021-09-30 23:19:11 -0600

    Created Login Page

--- Flask/market/models.py
+++ Flask/market/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import backref
 from market import db
+from market import bcrypt
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -8,6 +9,14 @@ class User(db.Model):
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=15)
     items = db.relationship('Champion', backref='owned_user', lazy=True)
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
 
 class Champion(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
